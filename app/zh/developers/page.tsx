25efab6ac954253cd3c604839a9fcd060937b08c
zhengzhize990328-byte <zhengzhize990328@gmail.com>
2026-09-05 12:44:50 +0800

    Add SIGNAL source: app/zh/developers
    Upload source files for the SIGNAL Global Risk Intelligence Platform

---
+++ app/zh/developers/page.tsx
@@ -0,0 +1,2 @@
+import MarketingStatusPage from '@/components/marketing-status-page'
+export default function ChineseDevelopersPage() { return <MarketingStatusPage locale='zh' kind='developers' /> }
